2fec982b60e68c167cdde77a95e50e9f9e93f3ec
Caleb Miller <caleb@mathmammoth.com>
2021-10-16 09:37:37 -0600

    commit

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p1pme4h=@r5qh24afydz*qp2tojh^0ej@g08g!*_3j@cp$(uyt'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(getenv('IS_DEVELOPMENT', "1")))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [getenv("APP_HOST")]
 
 
 # Application definition
@@ -120,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
 # Default primary key field type
